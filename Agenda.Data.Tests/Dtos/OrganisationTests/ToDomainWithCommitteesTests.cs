0ea71efffc8b504568cb1ffce669d37137d7089c
Steve Wright <stevewrightuk@gmail.com>
2020-04-24 20:48:26 +0100

    Add unit test for OrganisationDto.ToDomainWithCommittees

---
+++ Agenda.Data.Tests/Dtos/OrganisationTests/ToDomainWithCommitteesTests.cs
@@ -0,0 +1,89 @@
+﻿// <copyright file="ToDomainWithCommitteesTests.cs" company="Do It Wright">
+// Copyright (c) Do It Wright. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using Agenda.Data.Dtos;
+using Agenda.Data.Tests.TestUtilities;
+using Agenda.Domain.DomainObjects.Committees;
+using Agenda.Domain.DomainObjects.Organisations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Agenda.Data.Tests.Dtos.OrganisationTests
+{
+    /// <summary>
+    /// Tests <see cref="OrganisationDto.ToDomainWithCommittees"/>.
+    /// </summary>
+    [TestClass]
+    public class ToDomainWithCommitteesTests
+    {
+        /// <summary>
+        /// Tests with valid list of committees.
+        /// </summary>
+        [TestMethod]
+        public void Test_With_Valid_List_Of_Committees()
+        {
+            // ARRANGE
+            Guid paramId = Guid.NewGuid();
+            const string paramCode = "CBC";
+            const string paramName = "County Bridge Club";
+
+            OrganisationDto organisationDto = new OrganisationDto(
+                id: paramId,
+                code: paramCode,
+                name: paramName);
+
+            IList<CommitteeDto> paramCommittees = new List<CommitteeDto>
+            {
+                new CommitteeDto(
+                    id: Guid.NewGuid(),
+                    organisationId: organisationDto.Id,
+                    name: "TSC",
+                    description: "Tournament Sub-Committee",
+                    organisation: organisationDto)
+            };
+
+            organisationDto.SetPrivatePropertyValue(
+                propName: nameof(OrganisationDto.Committees),
+                value: paramCommittees);
+
+            // ACT
+            IOrganisationWithCommittees organisationWithCommittees =
+                organisationDto.ToDomainWithCommittees();
+
+            // ASSERT
+            Assert.AreEqual(paramId, organisationWithCommittees.Id);
+            Assert.AreEqual(paramCode, organisationWithCommittees.Code);
+            Assert.AreEqual(paramName, organisationWithCommittees.Name);
+            Assert.IsNotNull(organisationWithCommittees.Committees);
+            Assert.AreEqual(1, organisationWithCommittees.Committees.Count);
+
+            ICommittee committee = organisationWithCommittees.Committees[0];
+            Assert.IsNotNull(committee);
+            Assert.IsNotNull(committee.Organisation);
+            Assert.AreEqual(paramId, committee.Organisation.Id);
+        }
+
+        /// <summary>
+        /// Tests that null list of committees throws exception.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Test_That_Null_List_Of_Committees_Throws_Exception()
+        {
+            // ARRANGE
+            Guid paramId = Guid.NewGuid();
+            const string paramCode = "CBC";
+            const string paramName = "County Bridge Club";
+
+            OrganisationDto organisationDto = new OrganisationDto(
+                id: paramId,
+                code: paramCode,
+                name: paramName);
+
+            // ACT
+            _ = organisationDto.ToDomainWithCommittees();
+        }
+    }
+}
--- Agenda.Data.Tests/TestUtilities/TestUtils.cs
+++ Agenda.Data.Tests/TestUtilities/TestUtils.cs
@@ -2,7 +2,11 @@
 // Copyright (c) Do It Wright. All rights reserved.
 // </copyright>
 
+using System;
+using System.Globalization;
+using System.Reflection;
 using Agenda.Data.DbContexts;
+using Agenda.Data.Tests.Resources;
 using Microsoft.EntityFrameworkCore;
 
 namespace Agenda.Data.Tests.TestUtilities
@@ -32,6 +36,44 @@ namespace Agenda.Data.Tests.TestUtilities
                 builder.UseSqlServer(TestUtils.ConnectionString);
                 return builder.Options;
             }
+        }
+
+        /// <summary>
+        /// Sets the private property value.
+        /// </summary>
+        /// <typeparam name="T">Property Type.</typeparam>
+        /// <param name="target">Target object.</param>
+        /// <param name="propName">Property name.</param>
+        /// <param name="value">Value to set.</param>
+        public static void SetPrivatePropertyValue<T>(
+            this object target,
+            string propName,
+            T value)
+        {
+            if (target == null)
+            {
+                throw new ArgumentNullException(nameof(target));
+            }
+
+            Type t = target.GetType();
+            if (t.GetProperty(propName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance) == null)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(propName),
+                    string.Format(
+                        CultureInfo.InvariantCulture,
+                        ExceptionResources.Property___WasNotFoundInType___,
+                        propName,
+                        target.GetType().FullName));
+            }
+
+            t.InvokeMember(
+                name: propName,
+                invokeAttr: BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.SetProperty | BindingFlags.Instance,
+                binder: null,
+                target: target,
+                args: new object[] { value },
+                culture: CultureInfo.InvariantCulture);
         }
     }
 }
