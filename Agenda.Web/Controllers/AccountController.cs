42f2211b8d8521d71e20903eb86d0b4abc003585
Steve Wright <stevewrightuk@gmail.com>
2020-09-15 21:16:36 +0100

    Add Identity

---
+++ Agenda.Web/Controllers/AccountController.cs
@@ -0,0 +1,110 @@
+﻿// <copyright file="AccountController.cs" company="Do It Wright">
+// Copyright (c) Do It Wright. All rights reserved.
+// </copyright>
+
+////using System;
+////using System.Threading.Tasks;
+////using Agenda.Utilities.Models.Whos;
+////using Agenda.Web.ViewModels.Account;
+////using Microsoft.AspNetCore.Identity;
+////using Microsoft.AspNetCore.Mvc;
+////using Microsoft.Extensions.Logging;
+
+////namespace Agenda.Web.Controllers
+////{
+////    /// <summary>
+////    /// Account Controller.
+////    /// </summary>
+////    /// <seealso cref="Microsoft.AspNetCore.Mvc.ControllerBase" />
+////    public class AccountController : ControllerBase
+////    {
+////        private readonly ILogger<AccountController> logger;
+////        private readonly UserManager<IdentityUser> userManager;
+////        private readonly SignInManager<IdentityUser> signInManager;
+
+////        /// <summary>
+////        /// Initializes a new instance of the <see cref="AccountController"/> class.
+////        /// </summary>
+////        /// <param name="logger">Logger.</param>
+////        /// <param name="userManager">User Manager.</param>
+////        /// <param name="signInManager">Sign In Manager.</param>
+////        /// <param name="isTestMode">Is Test Mode.</param>
+////        public AccountController(
+////            ILogger<AccountController> logger,
+////            UserManager<IdentityUser> userManager,
+////            SignInManager<IdentityUser> signInManager,
+////            bool isTestMode = false)
+////            : base(typeof(AccountController), isTestMode)
+////        {
+////            this.logger = logger;
+////            this.userManager = userManager;
+////            this.signInManager = signInManager;
+////        }
+
+////        /// <summary>
+////        /// Register New User.
+////        /// </summary>
+////        /// <returns>View.</returns>
+////        [HttpGet]
+////        public IActionResult Register()
+////        {
+////            return this.View();
+////        }
+
+////        /// <summary>
+////        /// SOMETHING.
+////        /// </summary>
+////        /// <param name="model">The model.</param>
+////        /// <returns>SOMETHING.ELSE.</returns>
+////        [HttpPost]
+////        public async Task<IActionResult> Register(RegisterViewModel model)
+////        {
+////            IWho who = this.Who(nameof(this.Register));
+
+////            RegisterViewModel model1 = model?.Copy();
+
+////            this.logger.LogDebug(
+////                "ENTRY {ActionName}(who, model) {@who} {@model}",
+////                who.ActionName,
+////                who,
+////                model1);
+
+////            if (model == null)
+////            {
+////                throw new ArgumentNullException(nameof(model));
+////            }
+
+////            if (this.ModelState.IsValid)
+////            {
+////                IdentityUser user = new IdentityUser
+////                {
+////                    UserName = model.Email,
+////                    Email = model.Email
+////                };
+
+////                IdentityResult result = await this.userManager.CreateAsync(
+////                    user: user,
+////                    password: model.Password)
+////                    .ConfigureAwait(false);
+
+////                if (result.Succeeded)
+////                {
+////                    await this.signInManager.SignInAsync(
+////                            user: user,
+////                            isPersistent: false)
+////                        .ConfigureAwait(false);
+////                    return this.ExitRedirectToAction(
+////                        this.logger,
+////                        this.RedirectToAction("Index", "Home"));
+////                }
+
+////                foreach (IdentityError error in result.Errors)
+////                {
+////                    this.ModelState.AddModelError(string.Empty, error.Description);
+////                }
+////            }
+
+////            return this.ExitView(this.logger, this.View(model));
+////        }
+////    }
+////}
---
+++ Agenda.Web/ViewModels/Account/RegisterViewModel.cs
@@ -0,0 +1,73 @@
+﻿// <copyright file="RegisterViewModel.cs" company="Do It Wright">
+// Copyright (c) Do It Wright. All rights reserved.
+// </copyright>
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Agenda.Web.ViewModels.Account
+{
+    /// <summary>
+    /// Register View Model.
+    /// </summary>
+    public class RegisterViewModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RegisterViewModel"/> class.
+        /// </summary>
+        public RegisterViewModel()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RegisterViewModel"/> class.
+        /// </summary>
+        /// <param name="email">Email.</param>
+        /// <param name="password">Password.</param>
+        /// <param name="confirmPassword">Confirmation Password.</param>
+        public RegisterViewModel(
+            string email,
+            string password,
+            string confirmPassword)
+        {
+            this.Email = email;
+            this.Password = password;
+            this.ConfirmPassword = confirmPassword;
+        }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password confirmation.
+        /// </summary>
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare(
+            nameof(Password),
+            ErrorMessage = "Password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        /// <summary>
+        /// Copies this instance.
+        /// </summary>
+        /// <returns>Copy.</returns>
+        public RegisterViewModel Copy()
+        {
+            return new RegisterViewModel(
+                email: this.Email,
+                password: "PASSWORD",
+                confirmPassword: this.Password == this.ConfirmPassword ? "MATCH" : "MISMATCH");
+        }
+    }
+}
